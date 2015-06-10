553e2276fe26cd2f09699ab1faa1d07761fce9fe
Charlie Barker <Charlie@Hallville.net>
2015-06-10 23:23:25 +0100

    Added Test For Bender Serializer

---
+++ TestXmlSerialisation/BenderSerializer.cs
@@ -0,0 +1,13 @@
+﻿
+using Bender;
+namespace TestXmlSerialisation
+{
+    public class BenderSerializer
+    {
+        public static string SerialiseToXml(ShoppingBasket basket)
+        {
+            return Serialize.Xml(basket, x => x.Serialization(y =>
+                y.AsSimpleType<CustomerType>()));
+        }
+    }
+}
--- TestXmlSerialisation/Tests.cs
+++ TestXmlSerialisation/Tests.cs
@@ -12,7 +12,16 @@ namespace TestXmlSerialisation
             var testBasket = BasketBuilder.CreateTestBasket();
             var xml = MsSerializer.SerialiseToXml(testBasket);
             Debug.WriteLine(xml);
-            Assert.AreEqual("", xml, "Serialised Xml does not match");
+            Assert.AreEqual(@"<?xml version=""1.0"" encoding=""utf-16""?><ShoppingBasket xmlns:xsi=""http://www.w3.org/2001/XMLSchema-instance"" xmlns:xsd=""http://www.w3.org/2001/XMLSchema""><Name>Test Basket</Name><ItemCount>4</ItemCount><LastThingAdded>Thing2</LastThingAdded><CustomerType /></ShoppingBasket>", xml, "Serialised Xml does not match");
+        }
+
+        [Test]
+        public void TestBenderSerialiser()
+        {
+            var testBasket = BasketBuilder.CreateTestBasket();
+            var xml = BenderSerializer.SerialiseToXml(testBasket);
+            Debug.WriteLine(xml);
+            Assert.AreEqual(@"<?xml version=""1.0"" encoding=""utf-8""?><ShoppingBasket><Name>Test Basket</Name><ItemCount>4</ItemCount><LastThingAdded>Thing2</LastThingAdded><CustomerType>Gold</CustomerType></ShoppingBasket>", xml, "Serialised Xml does not match");
         }
     }
 }
